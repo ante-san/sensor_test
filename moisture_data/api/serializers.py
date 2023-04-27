62bb8043a32775b4dc8fd1fe1fef1f217292a62b
Andreas Sandgren <asandgren@gmail.com>
2023-04-27 14:22:41 +0930

    adding get function for moisture_data

--- moisture_data/api/serializers.py
+++ moisture_data/api/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework import serializers
-from moisture_data.models import MoistureData
+from moisture_data.models import MoistureData, Plant
 
+
+# Moisture Data
 class AddMoistureDataSerializer(serializers.ModelSerializer):
 
     class Meta:
         fields = '__all__'
         model = MoistureData
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+    
+
+
+# Plants
+class AddPlant(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Plant
 
     def create(self, validated_data):
         instance = self.Meta.model(**validated_data)
--- moisture_data/api/views.py
+++ moisture_data/api/views.py
@@ -3,14 +3,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import AddMoistureDataSerializer
+from moisture_data.models import MoistureData, Plant
 
-class AddMoistureData(APIView):
+class MoistureData(APIView):
 
     def post(self, request):
-        data = request.data
         serializer = AddMoistureDataSerializer(data=request.data)
         if serializer.is_valid():
             moisture_data = serializer.save()
             if moisture_data:
                 json = serializer.data
                 return Response(json, status = status.HTTP_201_CREATED)
+            
+    def get(self, request):
+        moisture_data = MoistureData.objects.all()
+        serializer = AddMoistureDataSerializer(moisture_data, many=True)
+        return Response(serializer.data)
